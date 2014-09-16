5da52d36938010ad88695eb926febd2fbdf6e155
Michiko HIRATA <m-hirata@groovenauts.jp>
2014-09-16 17:37:49 +0900

    spec書いてみました（modelのみ）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'from_omniauth' do
+    it 'create' do
+      auth = OmniAuth::AuthHash.new
+      auth.provider = 'my provider'
+      auth.uid = 'my uid'
+      auth.info = {email: 'email@example.com', name: 'my name'}
+
+      new_user = User.from_omniauth(auth)
+
+      expect(new_user.provider).to eq(auth.provider)
+      expect(new_user.uid).to eq(auth.uid)
+      expect(new_user.email).to eq(auth.info.email)
+      expect(new_user.password).not_to be_nil
+      expect(new_user.password.size).to eq(20)
+      expect(new_user.role).to eq('admin')
+      expect(new_user.name).to eq(auth.info.name)
+    end
+
+    it 'create (nothing email)' do
+      auth = OmniAuth::AuthHash.new
+      auth.provider = 'my provider'
+      auth.uid = 'my uid'
+      auth.info = {email: '', name: 'my name'}
+
+      new_user = User.from_omniauth(auth)
+
+      expect(new_user.email).not_to be_nil
+    end
+
+  end
 end
